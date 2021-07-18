85e24822f3c922e760b8a227400b1b1506f015fa
Thunraz <thunraz@thunraz.com>
2021-07-18 10:50:42 +0200

    Implement iterative bisect algorithm

--- src/bisection.cpp
+++ src/bisection.cpp
@@ -1,16 +1,23 @@
 #include "bisection.hpp"
 #include "sgn.hpp"
 
+#include <cstdlib>
 
-double calculate_mean(double L, double U)
-{
-    // fake implementation
-    return 42.0;
-}
+double calculate_mean(double L, double U) { return (L + U) / 2.0; }
 
 double bisect(std::function<double(double)> const& f, double L, double U, double delta)
 {
-    // fake implementation
-    return 42.0;
-}
+    while (true) {
+        auto const mean = calculate_mean(L, U);
+        auto const function_value = f(mean);
+        if (abs(function_value) <= delta) {
+            return mean;
+        }
 
+        if (sgn(function_value) == sgn(f(L))) {
+            L = mean;
+        } else {
+            U = mean;
+        }
+    }
+}
