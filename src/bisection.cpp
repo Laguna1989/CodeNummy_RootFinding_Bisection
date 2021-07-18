c51ba2dc4287974cc5e19658b7a82a0d4f6e1b6c
Simon Weis <laguna_1989@gmx.net>
2021-07-18 08:31:21 +0200

    Add solution for bisection algorithm

--- src/bisection.cpp
+++ src/bisection.cpp
@@ -1,16 +1,21 @@
 #include "bisection.hpp"
 #include "sgn.hpp"
 
-
-double calculate_mean(double L, double U)
-{
-    // fake implementation
-    return 42.0;
-}
+double calculate_mean(double L, double U) { return (L + U) / 2.0; }
 
 double bisect(std::function<double(double)> const& f, double L, double U, double delta)
 {
-    // fake implementation
-    return 42.0;
-}
+    double const M = calculate_mean(L, U);
+    double const vM = f(M);
+    if (abs(vM) <= delta) {
+        return M;
+    }
 
+    if (sgn(f(M)) == sgn(f(L))) {
+        L = M;
+    } else {
+        U = M;
+    }
+
+    return bisect(f, L, U, delta);
+}
